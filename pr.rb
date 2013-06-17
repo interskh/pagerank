3e02b6ab8d69747ed6f4238a8e38ff1820452a5f
Kyle Sun <interskh@gmail.com>
2013-06-17 16:20:03 -0700

    to play safe, set the time out to be 1-3 sec

--- pr.rb
+++ pr.rb
@@ -4,7 +4,7 @@ require 'page_rankr'
 get '/*' do
   url = params[:splat].join('/')
   begin
-    sleep 0.1+rand
+    sleep 1+rand*2 #just to play safe, 1 to 3 sec
     pr = PageRankr.ranks(url, :google)
     rank = pr[:google]
     rank = 0 if rank.nil?
